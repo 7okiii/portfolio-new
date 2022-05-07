dfd3a4e00b08c9326d84cd6884a4e52f03a1537e
Nnookk <naokiogata@NaokinoAir.com>
2022-05-06 22:47:43 -0400

    will add more animation later

--- js/index.js
+++ js/index.js
@@ -42,3 +42,65 @@ $(window).scroll(function() {
     }
     startPos = currentPos;
 })
+
+// fade-in animation when scroll down
+var window_h = $(window).height();
+$(window).on("scroll",function() {
+    var scroll_top = $(window).scrollTop();
+    $('.fadeInLeft').each(function() {
+        var elem_pos = $(this).offset().top;
+        if (scroll_top >= elem_pos - window_h + 300) {
+            $(this).addClass('fadeInFromLeft');
+        } else {
+            // $(this).removeClass('fadeInFromLeft');
+        }
+    });
+    //
+    $('.fadeInRight').each(function() {
+        var elem_pos = $(this).offset().top;
+        if (scroll_top >= elem_pos - window_h + 300) {
+            $(this).addClass('fadeInFromRight');
+        } else {
+            // $(this).removeClass('fadeInFromRight');
+        }
+    });
+    //
+    $('.fadeInBottom').each(function() {
+        var elem_pos = $(this).offset().top;
+        if (scroll_top >= elem_pos - window_h + 300) {
+            $(this).addClass('fadeInFromBottom');
+        } else {
+            // $(this).removeClass('fadeInFromBottom');
+        }
+    });
+    //for project section
+    $('.fadeInOrange1').each(function() {
+        var elem_pos = $(this).offset().top;
+        if (scroll_top >= elem_pos - window_h + 300) {
+            $(this).addClass('fadeInFromLeft');
+        } else {
+            // $(this).removeClass('fadeInFromBottom');
+        }
+    });
+    // $('.fadeInBlue').each(function() {
+    //     var elem_pos = $(this).offset().top;
+    //     if (scroll_top >= elem_pos - window_h + 300) {
+    //         $(this).addClass('fadeInFromRight');
+    //     } else {
+    //         // $(this).removeClass('fadeInFromBottom');
+    //     }
+    // });
+    $('.fadeInOrange2').each(function() {
+        var elem_pos = $(this).offset().top;
+        if (scroll_top >= elem_pos - window_h + 300) {
+            $(this).addClass('fadeInFromLeft');
+        } else {
+            // $(this).removeClass('fadeInFromBottom');
+        }
+    });
+});
+
+//going back to top of the page
+$('#arrowToTop').on('click',function() {
+    window.scrollTo({top:0, behavior: 'smooth'});
+})
