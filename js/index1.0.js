739c5b2afafc7fb582911f68f8ced09ebd3b3931
Nnookk <naokiogata@NaokinoAir.com>
2022-05-13 21:55:43 -0400

    some changes

--- js/index1.0.js
+++ js/index1.0.js
@@ -104,3 +104,62 @@ $(window).on("scroll",function() {
 $('#arrowToTop').on('click',function() {
     window.scrollTo({top:0, behavior: 'smooth'});
 })
+
+let readMoreBtn1 = document.getElementById('read-more1');
+let readMoreBtn2 = document.getElementById('read-more2');
+let readMoreBtn3 = document.getElementById('read-more3');
+let readMoreDetail1 = document.getElementById('read-more-detail1');
+let readMoreDetail2 = document.getElementById('read-more-detail2');
+let readMoreDetail3 = document.getElementById('read-more-detail3');
+
+
+readMoreBtn1.addEventListener('click',function() {
+    if (readMoreDetail1.style.display === 'block') {
+        readMoreDetail1.style.display = 'none';
+        readMoreBtn1.innerHTML = 'READ MORE'
+    } else {
+        readMoreDetail1.style.display = 'block';
+        readMoreBtn1.innerHTML = 'HIDE'
+    }
+})
+
+readMoreBtn2.addEventListener('click',function() {
+    if (readMoreDetail2.style.display === 'block') {
+        readMoreDetail2.style.display = 'none';
+        readMoreBtn2.innerHTML = 'READ MORE'
+    } else {
+        readMoreDetail2.style.display = 'block';
+        readMoreBtn2.innerHTML = 'HIDE'
+    }
+})
+
+readMoreBtn3.addEventListener('click',function() {
+    if (readMoreDetail3.style.display === 'block') {
+        readMoreDetail3.style.display = 'none';
+        readMoreBtn3.innerHTML = 'READ MORE'
+    } else {
+        readMoreDetail3.style.display = 'block';
+        readMoreBtn3.innerHTML = 'HIDE'
+    }
+})
+
+
+// $('.read-more').on('click',function() {
+//     if ($('.read-more-detail').css('display') == 'none') {
+//         $('.read-more-detail').css('display','block');
+//         $('.read-more').html('HIDE');
+//     } else {
+//         $('.read-more-detail').css('display','none');
+//     }
+//     // $('.read-more-detail').css('display','block');
+//     console.log($('.read-more-detail').css('display'));
+// })
+
+// let readMoreBtn = document.getElementsByClassName('read-more');
+// let readMoreDetail = document.getElementsByClassName('read-more-detail');
+
+// for (var i = 0; i < readMoreBtn.length; i++) {
+//     readMoreBtn[i].addEventListener('click',function() {
+//         console.log(readMoreDetail[0].display);
+//     })
+// }
